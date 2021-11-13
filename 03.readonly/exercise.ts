33a195b82e44c0e174521ff3bfb6674cce1bd643
j <2019292338@qq.com>
2021-11-13 21:33:06 +0800

    feat: 03.readonly exercises

---
+++ 03.readonly/exercise.ts
@@ -0,0 +1,17 @@
+type MyReadonly<T> = {
+    readonly [key in keyof T]: T[key]
+}
+
+/** test case */
+// interface Todo {
+//     title: string
+//     description: string
+// }
+
+// const todo: MyReadonly<Todo> = {
+//     title: 'Hey',
+//     description: 'foobar',
+// }
+
+// todo.title = 'Hello' // Error: cannot reassign a readonly property
+// todo.description = 'barFoo' // Error: cannot reassign a readonly property
