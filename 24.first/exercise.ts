10e366da95156c08e8868496d4b2430a116f228e
j <2019292338@qq.com>
2021-11-16 22:54:36 +0800

    feat: 24.first exercise

---
+++ 24.first/exercise.ts
@@ -0,0 +1,8 @@
+type First<U extends unknown[]> = U extends [infer first, ...unknown[]] ? first : never 
+
+/** test case */
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
