d65c9a98b51371e10904907244a23196e5ed7f77
j <2019292338@qq.com>
2021-11-16 22:52:45 +0800

    feat: 23.last exercise

---
+++ 23.last/exercise.ts
@@ -0,0 +1,8 @@
+type Last<U extends unknown[]> = U extends [...unknown[], infer last] ? last : never 
+
+/** test case */
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type tail1 = Last<arr1> // expected to be 'c'
+type tail2 = Last<arr2> // expected to be 1
