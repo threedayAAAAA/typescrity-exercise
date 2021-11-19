087a1a6e69097f226f90810d7a146ace9bb0d076
j <2019292338@qq.com>
2021-11-19 22:02:08 +0800

    52.endWidth exercise

--- 51.startWitdh/exercise.ts
+++ 51.startWitdh/exercise.ts
@@ -1,6 +1,6 @@
-type StartsWith<T, U extends string> = T extends `${U}${any}` ? true : false
+type EndsWith<T, U extends string> = T extends `${any}${U}` ? true : false
 
 /** test case */
-type a = StartsWith<'abc', 'ac'> // expected to be false
-type b = StartsWith<'abc', 'ab'> // expected to be true
-type c = StartsWith<'abc', 'abcd'> // expected to be false
+type a2 = EndsWith<'abc', 'ac'> // expected to be false
+type b2 = EndsWith<'abc', 'bc'> // expected to be true
+type c2 = EndsWith<'abc', 'dabc'> // expected to be false
